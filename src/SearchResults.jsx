fb2f88a896ff26fdfe762c7092cb64cb4b81394d
Dhanuka Gunarathna <nuwan.dhanuka@gmail.com>
2023-03-18 17:38:54 +0100

    basic API call

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import useSWR from "swr";
 
 const SearchResults = (props) => {
+
+    const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+    // fetch data
+    const { data, error } = useSWR(
+        "https://developer.wordpress.org/wp-json/wp/v2/comments",
+        fetcher
+    );
+
+    if (error) return <div>failed to load</div>;
+    if (!data) return <div>loading...</div>;
+
     return(
         <div className="results-wrap">
-            <h2>Search results for: {props?.query}</h2>
+            <h2 className="mb-4">Search results for: {props?.query}</h2>
+            
+            { data && data?.length > 0 && props?.query && props?.query.length > 2 &&
+                <>
+                    {data.map( ( item, i ) => {
+                        return (
+                            <div key={i}>
+                                <div dangerouslySetInnerHTML={{__html: item?.content?.rendered}} />
+                            </div>
+                        )
+                    })}
+                </>
+            }
+
         </div>
     );
 }
