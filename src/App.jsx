06216230d7961aa3db4bdae21a142743e1f1851e
Luke Carbis <luke@carb.is>
2023-03-19 10:24:33 +0100

    Make it pretty

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import "./main.css";
 import SearchBar from "./SearchBar";
 
 const App = () => {
   return (
     <div className="App">
-      	<Helmet>
-			<meta name="description" content="A search engine for WordPress developers"></meta>
-			<title>docs_dangit</title>
-		</Helmet>
+      <Helmet>
+        <meta
+          name="description"
+          content="A search engine for WordPress developers"
+        ></meta>
+        <title>docs_dangit</title>
+      </Helmet>
 
-					<>
-						<h1 className="">docs_dangit: A search engine for WordPress developers</h1>
-					</>
+      <h1 className="">
+        docs_dangit: A search engine for WordPress developers
+      </h1>
 
-				<>
-					<SearchBar />
-				</>
-
+      <SearchBar />
     </div>
   );
-}
+};
 
 export default App;
--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -2,37 +2,37 @@ import React from "react";
 import useSWR from "swr";
 
 const SearchResults = (props) => {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-    const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  // fetch data
+  const { data, error } = useSWR(
+    "https://developer.wordpress.org/wp-json/wp/v2/comments",
+    fetcher
+  );
 
-    // fetch data
-    const { data, error } = useSWR(
-        "https://developer.wordpress.org/wp-json/wp/v2/comments",
-        fetcher
-    );
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
 
-    if (error) return <div>failed to load</div>;
-    if (!data) return <div>loading...</div>;
+  return (
+    <div className="results-wrap">
+      <h2>Search results for: {props?.query}</h2>
 
-    return(
-        <div className="results-wrap">
-            <h2>Search results for: {props?.query}</h2>
-            
-            { data && data?.length > 0 && props?.query && props?.query.length > 2 &&
-                <>
-                    {console.log( data )}
-                    {data.map( ( item, i ) => {
-                        return (
-                            <div key={i}>
-                                <div dangerouslySetInnerHTML={{__html: item?.content?.rendered}} />
-                            </div>
-                        )
-                    })}
-                </>
-            }
-
-        </div>
-    );
-}
+      {data && data?.length > 0 && props?.query && props?.query.length > 2 && (
+        <>
+          {console.log(data)}
+          {data.map((item, i) => {
+            return (
+              <div key={i}>
+                <div
+                  dangerouslySetInnerHTML={{ __html: item?.content?.rendered }}
+                />
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default SearchResults;
