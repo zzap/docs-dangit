53fffc6ae11989177a1e835c60df291b2e2dd198
Dhanuka Gunarathna <nuwan.dhanuka@gmail.com>
2023-03-19 18:17:04 +0100

    add logo a link

--- src/svg/DocsDangitIconBold.js
+++ src/svg/DocsDangitIconBold.js
@@ -13,7 +13,7 @@ export default function DocsDangitIcon(props) {
         cy="110"
         r="95"
         stroke="currentColor"
-        stroke-width="30"
+        strokeWidth="30"
       />
       <path
         d="M235.476 256.689C241.379 262.501 250.876 262.427 256.689 256.524C262.501 250.621 262.427 241.124 256.524 235.312L235.476 256.689ZM170.476 192.689L235.476 256.689L256.524 235.312L191.524 171.311L170.476 192.689Z"
@@ -22,8 +22,8 @@ export default function DocsDangitIcon(props) {
       <path
         d="M127 75L162 110"
         stroke="currentColor"
-        stroke-width="20"
-        stroke-linecap="round"
+        strokeWidth="20"
+        strokeLinecap="round"
       />
       <path
         d="M119.788 138.073C115.882 141.978 115.946 148.245 119.929 152.071C123.912 155.897 130.307 155.832 134.212 151.927L119.788 138.073ZM134.212 151.927L169.212 116.927L154.788 103.073L119.788 138.073L134.212 151.927Z"
@@ -32,8 +32,8 @@ export default function DocsDangitIcon(props) {
       <path
         d="M92 145L57 110"
         stroke="currentColor"
-        stroke-width="20"
-        stroke-linecap="round"
+        strokeWidth="20"
+        strokeLinecap="round"
       />
       <path
         d="M99.2124 81.9268C103.118 78.0216 103.054 71.7545 99.0711 67.9289C95.0878 64.1034 88.6928 64.1679 84.7876 68.0732L99.2124 81.9268ZM84.7876 68.0732L49.7876 103.073L64.2124 116.927L99.2124 81.9268L84.7876 68.0732Z"
