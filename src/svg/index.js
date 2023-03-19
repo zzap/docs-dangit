ca8492a025ee9058af1028121adcdbf8c7e04d44
Jessica Lyschik <luminuu@gmail.com>
2023-03-19 13:02:57 +0100

    Add copy icon

--- src/svg/index.js
+++ src/svg/index.js
@@ -1,2 +1,3 @@
 export { default as DocsDangitIcon } from "./DocsDangitIcon";
 export { default as DocsDangitIconBold } from "./DocsDangitIconBold";
+export { default as CopyIcon } from "./CopyIcon";
