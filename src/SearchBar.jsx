a79afb34c55fba4cac558a00e9864644464d8916
Kevin Batdorf <kbat82@gmail.com>
2023-03-19 12:02:53 +0100

    Use tabs

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -2,28 +2,28 @@ import React, { useState } from "react";
 import SearchResults from "./SearchResults";
 
 const SearchBar = () => {
-  const [searchQuery, setSearchQuery] = useState("");
+	const [searchQuery, setSearchQuery] = useState("");
 
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-  };
+	const handleSearch = (query) => {
+		setSearchQuery(query);
+	};
 
-  return (
-    <div className="search-bar-wrap">
-      <form action="/" method="post">
-        <input
-          className="w-full rounded-full py-4 px-6 border-2 font-mono"
-          type="search"
-          name="s"
-          value={searchQuery}
-          onChange={(e) => handleSearch(e.target.value)}
-          placeholder="Start typing..."
-        />
-      </form>
+	return (
+		<div className="search-bar-wrap">
+			<form action="/" method="post">
+				<input
+					className="w-full rounded-full py-4 px-6 border-2 font-mono"
+					type="search"
+					name="s"
+					value={searchQuery}
+					onChange={(e) => handleSearch(e.target.value)}
+					placeholder="Start typing..."
+				/>
+			</form>
 
-      <SearchResults query={searchQuery} />
-    </div>
-  );
+			<SearchResults query={searchQuery} />
+		</div>
+	);
 };
 
 export default SearchBar;
--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -3,42 +3,47 @@ import Highlight from "react-highlight";
 import useSWR from "swr";
 
 const SearchResults = (props) => {
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+	const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  // fetch data
-  const { data, error } = useSWR(
-    "https://developer.wordpress.org/wp-json/wp/v2/comments",
-    fetcher
-  );
+	// fetch data
+	const { data, error } = useSWR(
+		"https://developer.wordpress.org/wp-json/wp/v2/comments",
+		fetcher
+	);
 
-  if (error) return <p className="text-gray-600">Failed to load, dangit.</p>;
-  if (!data) return <p className="text-gray-600">Loading...</p>;
+	if (error) return <p className="text-gray-600">Failed to load, dangit.</p>;
+	if (!data) return <p className="text-gray-600">Loading...</p>;
 
-  return (
-    <div className="results-wrap mt-8">
-      {props?.query &&
-        (props?.query.length < 3 ? (
-          <p className="text-gray-600">Keep typing...</p>
-        ) : (
-          <h2 className="font-bold">Search results for: {props?.query}</h2>
-        ))}
-      {data && data?.length > 0 && props?.query && props?.query.length > 2 && (
-        <div className="grid grid-cols-3 gap-4 mt-4">
-          {console.log(data)}
-          {data.map((item, i) => {
-            return (
-              <Highlight
-                key={i}
-                className="html rounded-xl p-4 shadow font-mono h-60 text-sm overflow-hidden whitespace-pre-wrap"
-              >
-                {item?.content?.rendered}
-              </Highlight>
-            );
-          })}
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className="results-wrap mt-8">
+			{props?.query &&
+				(props?.query.length < 3 ? (
+					<p className="text-gray-600">Keep typing...</p>
+				) : (
+					<h2 className="font-bold">
+						Search results for: {props?.query}
+					</h2>
+				))}
+			{data &&
+				data?.length > 0 &&
+				props?.query &&
+				props?.query.length > 2 && (
+					<div className="grid grid-cols-3 gap-4 mt-4">
+						{console.log(data)}
+						{data.map((item, i) => {
+							return (
+								<Highlight
+									key={i}
+									className="html rounded-xl p-4 shadow font-mono h-60 text-sm overflow-hidden whitespace-pre-wrap"
+								>
+									{item?.content?.rendered}
+								</Highlight>
+							);
+						})}
+					</div>
+				)}
+		</div>
+	);
 };
 
 export default SearchResults;
