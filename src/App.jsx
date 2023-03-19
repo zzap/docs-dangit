32503e404231ef98d3540387120259b30d41ce21
Luke Carbis <luke@carb.is>
2023-03-19 11:54:37 +0100

    Style the tagline

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,9 @@ const App = () => {
           <div className="flex flex-wrap md:flex-nowrap justify-items-start items-center gap-4">
             <DocsDangitIcon className="w-8 h-8 text-white" />
             <h1 className="mr-2 font-bold">docs_dangit</h1>
-            <span className="">A search engine for WordPress developers</span>
+            <span className="font-mono ml-4 mb-1 text-sm self-end">
+              A search engine for WordPress developers
+            </span>
           </div>
 
           <div className="flex flex-wrap lg:flex-nowrap gap-3">
@@ -45,9 +47,9 @@ const App = () => {
       </header>
 
       <main className="p-8">
-		<div className="w-full max-w-7xl mx-auto">
-        	<SearchBar />
-		</div>
+        <div className="w-full max-w-7xl mx-auto">
+          <SearchBar />
+        </div>
       </main>
     </div>
   );
