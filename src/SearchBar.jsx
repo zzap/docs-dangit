fbaee42ca7dd336f98e17e922923ed4f874da7a2
Kevin Batdorf <kbat82@gmail.com>
2023-03-20 11:00:04 +0100

    Add type filtering

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { useEffect, useState } from "react";
 import SearchResults from "./SearchResults";
 import { useSearchStore } from "./state/search";
@@ -6,8 +7,16 @@ const capitalPDangit = (query) => {
   return query.replace(/Wordpress/i, "WordPress");
 };
 
+const types = {
+  '': 'All',
+  wordpress_reference: 'WordPress Reference',
+  wordpress_dev_reference: 'WordPress Dev Notes',
+  wpcli: 'WP CLI',
+  php_reference: 'PHP.net',
+}
+
 const SearchBar = () => {
-  const { search, setSearch, setSearchHistory } = useSearchStore();
+  const { search, setSearch, setSearchHistory, type, setType } = useSearchStore();
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedResult, selectResult] = useState(null);
 
@@ -31,7 +40,10 @@ const SearchBar = () => {
 
   return (
     <div className="search-bar-wrap">
-      <form method="get" onSubmit={(e) => e.preventDefault()}>
+      <form className="flex flex-col gap-3" method="get" onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="search" className="sr-only">
+          Search
+        </label>
         <input
           className="w-full rounded-full py-4 px-6 border-2 font-mono"
           type="search"
@@ -41,10 +53,22 @@ const SearchBar = () => {
           placeholder="Search for a WordPress function, hook, or class."
           autoFocus
         />
+        <div className="flex gap-2 items-center">
+          <p className="text-sm">Filter by type:</p>
+          {Object.entries(types).map(([key, value]) => (
+            <button type="button" onClick={() => setType(key)} className={clsx("p-1 px-3 rounded-xl text-sm", {
+              'bg-blue-700 text-white': type === key,
+              'bg-gray-200': type !== key,
+            })}>
+              {value}
+            </button>
+          ))}
+        </div>
       </form>
 
       <SearchResults
-        query={search}
+        search={search}
+        type={type}
         selectedResult={selectedResult}
         selectResult={selectResult}
       />
