b2788b1f7e644f9b5a76213b0b2020fb0cc3b53e
Luke Carbis <luke@carb.is>
2023-03-20 11:14:30 +0100

    Subtle text color change

--- src/LanguageTag.jsx
+++ src/LanguageTag.jsx
@@ -1,7 +1,7 @@
 const LanguageTag = (props) => {
   const { language } = props;
   return (
-    <div className="flex items-end justify-end text-white text-sm font-mono p-4 h-full">
+    <div className="flex items-end justify-end text-gray-100 text-sm font-mono p-4 h-full">
       <span className="bg-gray-500/80 p-2 rounded-lg">{language}</span>
     </div>
   );
