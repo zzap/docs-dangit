693103eaae350dbfabaa665d812193cfc1593f18
David Beja <david.beja@gmail.com>
2023-03-31 18:36:12 +0100

    Improve cli help

--- cli/src/Command/Ingest.php
+++ cli/src/Command/Ingest.php
@@ -31,15 +31,9 @@ class Ingest extends Command
             ->setDescription("Ingest docs")
             ->setDefinition(
                 new InputDefinition([
-                    new InputOption('source', 's', InputOption::VALUE_OPTIONAL)
+                    new InputOption('source', 's', InputOption::VALUE_OPTIONAL, 'Docs source (wp-docs, php-docs, wp-cli, wp-dev). All sources by default.')
                 ])
-            )
-            ->setHelp(<<<EOT
-Ingest docs from different sources.
-
-Usage:
-<info>docsdangit ingest</info>
-EOT);
+            );
     }
 
     /**
@@ -52,22 +46,22 @@ EOT);
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $source = $input->getOption('source');
-        if(!$source || 'wp-docs' === $source ) {
+        if( ! $source || 'wp-docs' === $source ) {
             $output->writeln('🚀 Ingesting WordPress Docs...');
             $wp_docs = new WordPress_Docs();
             $wp_docs->parse();
         }
-        if(!$source || 'wp-cli' === $source ) {
+        if( ! $source || 'wp-cli' === $source ) {
             $output->writeln('🚀 Ingesting WP CLI Docs...');
             $wp_docs = new WP_CLI();
             $wp_docs->parse();
         }
-        if(!$source || 'php-docs' === $source ) {
+        if( ! $source || 'php-docs' === $source ) {
             $output->writeln('🚀 Ingesting PHP Docs...');
             $wp_docs = new PHP_Docs();
             $wp_docs->parse();
         }
-        if(!$source || 'wp-dev' === $source ) {
+        if( ! $source || 'wp-dev' === $source ) {
             $output->writeln('🚀 Ingesting WordPress Dev Blog Docs...');
             $wp_docs = new Make_WordPress();
             $wp_docs->parse();
